e51a899fbcf55d623871f80e6dcdd7e9b8725c70
rbnograles <ryannograles.025.gtsi@gmail.com>
2022-10-21 19:17:03 +0800

    Add: a very big sum problem

---
+++ src/AVeryBigSum.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AVeryBigSum {
+    public static void main(String[] args) {
+        List<Long> ar = new ArrayList<>();
+        long sum = 0;
+        for(long l: ar) {
+            sum += l;
+        }
+        System.out.println(sum);
+    }
+}

cf6397d20830f912efe2c9520e74731a7598f4b6
rbnograles <ryannograles.025.gtsi@gmail.com>
2022-10-28 08:28:00 +0800

    Added migratory bird problem

---
+++ src/MigratoryBirds.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
+public class MigratoryBirds {
+    public static void main(String[] args) {
+        List<Integer> arr = new ArrayList<>();
+
+        arr.add(1);
+        arr.add(1);
+        arr.add(2);
+        arr.add(2);
+        arr.add(3);
+
+        HashMap<Integer, Integer> birdTypes = new HashMap<>();
+
+        int counter = 0;
+        int numberType = Integer.MIN_VALUE;
+
+        for(int t : arr) {
+           if(birdTypes.containsKey(t)) {
+               // add one to the type count
+               birdTypes.put(t, birdTypes.get(t) + 1);
+           } else  {
+               // place 1 count on the type
+               birdTypes.put(t, 1);
+           }
+        }
+        // get keys from the hashmap
+        Set<Integer> keySet = birdTypes.keySet();
+        // loop for each keys on the hashmap
+        for(int i: keySet) {
+            if(birdTypes.get(i) > counter) {
+                numberType = i;
+                counter = birdTypes.get(i);
+            }
+        }
+
+        System.out.println(numberType);
+    }
+}
